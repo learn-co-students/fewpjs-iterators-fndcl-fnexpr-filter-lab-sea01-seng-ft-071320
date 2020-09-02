cd8f834a64d789ec6650c71bc1042de541a43ffa
cortezd334 <cortezd334@gmail.com>
2020-09-02 11:14:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(name => {
+        if(name.toLowerCase() === string.toLowerCase())
+        return name
+    })
+}
+findMatching(drivers, name)
+
+function fuzzyMatch(array, string) {
+    return array.filter(name => {
+        if(name.startsWith(string))
+        return name
+    })
+}
+fuzzyMatch(drivers, string)
+
+function matchName(array, string) {
+    return array.filter(driver => {
+        if(driver.name === string)
+        return driver
+    })
+}
+matchName(drivers, string)
